adee888a72bcae06d42ecbaf7cdfb5eec25a09bc
Connor Gray <accounts@connorgray.com>
2016-09-09 12:46:24 -0500

    Created tests module and moved existings tests there

--- src/create/mod.rs
+++ src/create/mod.rs
@@ -520,39 +520,3 @@ fn print_regexp_depth(regexp: &Regexp, depth: i64) {
             }
     }
 }
-
-#[test]
-fn test_regexp_parser() {
-    let regexp_strings = vec![
-        "(a+b)",
-        "(a|b)|(c|d)",
-        "((a|b)|(c|d)+)+",
-        "((a|b))",
-        "((a))",
-        "((a)",
-        "(a))",
-        "((a|b)b|(c+d|q))+",
-        "(abc)(123)+",
-    ];
-}
-
-#[test]
-fn test_regexs_error_detection() {
-    let regexp_strings = vec![
-        "a+b",
-        "((a|b)|(c|d))",
-        "((a|b)|(c|d)+)+",
-        "(a|b)",
-        "a",
-        "((a)",
-        "(a))",
-        "((a|b)b|(c+d|q))+",
-    ];
-
-    for regexp_string in regexp_strings.iter() {
-        let regexp = regexp_from_string(&regexp_string);
-        let output_string = regexp_to_string(&regexp);
-
-        assert_eq!(**regexp_string, *output_string);
-    }
-}
